fe80c92110284d8343fb11d6904c5377985b0165
William S. Rodriguez <williamsrz@outlook.com>
2017-03-26 20:48:23 -0300

    ajuste swagger bootstrap

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace MonkeyHubApi
 
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Monkey Hub API");
             });
 
             app.UseMvc();
